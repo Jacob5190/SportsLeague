ef9a7ef78886918486e15b9dff5948c8d46561b0
BuildTools <unconfigured@null.spigotmc.org>
2020-06-02 10:18:47 +0800

    Upload files to github

---
+++ src/controller/calendarController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class calendarController{
+	@RequestMapping("/calendar")
+	public ModelAndView handleRequest (javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+		return new ModelAndView("calendar.jsp");
+	}
+	@RequestMapping("/eventUpload")
+	@ResponseBody
+	public void eventUpload (@RequestParam("date") String iDate, @RequestParam("Event") String iEvent) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(iDate);
+
+	}
+}
